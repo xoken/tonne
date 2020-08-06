70f0aeef4348658c0c5022662bf091bc0b53c69d
shubendrak <shubendrak@gmail.com>
2020-08-06 12:55:34 +0530

    Add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,23 @@ import Login from './Login';
 import logo from './images/logo.png';
 export default class App extends React.Component {
   componentDidMount() {
-    this.addScript('./scripts/index.js');
-    this.addScript('./scripts/credentials.js');
-    this.addScript('./scripts/auth.js');
-    this.addScript('./scripts/httpsreq.js');
-    this.addScript('./scripts/address.js');
-    this.addScript('./scripts/transaction.js');
-    this.addScript('./scripts/blockheight.js');
-    this.addScript('./scripts/mnemonic.js');
-    this.addScript('./scripts/search.js');
-    this.addScript('./scripts/wallet.js');
+    // this.addScript('./scripts/index.js');
+    // this.addScript('./scripts/credentials.js');
+    // this.addScript('./scripts/auth.js');
+    // this.addScript('./scripts/httpsreq.js');
+    // this.addScript('./scripts/address.js');
+    // this.addScript('./scripts/transaction.js');
+    // this.addScript('./scripts/blockheight.js');
+    // this.addScript('./scripts/mnemonic.js');
+    // this.addScript('./scripts/search.js');
+    // this.addScript('./scripts/wallet.js');
   }
 
-  addScript(scriptSrc: string) {
-    const script = document.createElement('script');
-    script.src = scriptSrc;
-    document.body.appendChild(script);
-  }
+  // addScript(scriptSrc: string) {
+  //   const script = document.createElement('script');
+  //   script.src = scriptSrc;
+  //   document.body.appendChild(script);
+  // }
 
   render() {
     return (
