d347c1b753d6a7ecc7e46f2c0c776f6695899fe3
shubendrak <shubendrak@gmail.com>
2020-08-14 11:12:38 +0530

    update mockup

---
+++ build/precache-manifest.06f2f7837c77c1491dc2c45821df5112.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55996eeec8790ba236b2d43d7946a1af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b542133ff1d0fa0ed19a",
+    "url": "./static/css/main.5489b4ab.chunk.css"
+  },
+  {
+    "revision": "c17e8d15d5d43fe01835",
+    "url": "./static/js/2.000c6aff.chunk.js"
+  },
+  {
+    "revision": "c7f92eccde8e496c47f87d08777731a5",
+    "url": "./static/js/2.000c6aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b542133ff1d0fa0ed19a",
+    "url": "./static/js/main.85262d15.chunk.js"
+  },
+  {
+    "revision": "524eedc2b001f48a380f",
+    "url": "./static/js/runtime-main.e69a4097.js"
+  },
+  {
+    "revision": "5c81a1d6e7fcdc23c3b3a04ca8fba6bd",
+    "url": "./static/media/logo.5c81a1d6.png"
+  }
+]);
