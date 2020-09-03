e295f03316aaa4b45bd8f93ba793dbe83e82f8a8
shubendrak <shubendrak@gmail.com>
2020-09-04 01:56:30 +0530

    send transaction

--- lib/nipkow-sdk/src/Wallet.ts
+++ lib/nipkow-sdk/src/Wallet.ts
@@ -1,10 +1,20 @@
-import { Network, bip32, ECPair, payments } from 'bitcoinjs-lib';
+import {
+  Network,
+  bip32,
+  ECPair,
+  payments,
+  Psbt,
+  networks,
+} from 'bitcoinjs-lib';
 import * as bip38 from 'bip38';
 import * as bip39 from 'bip39';
+import coinSelect from 'coinselect';
+import pako from 'pako';
 import * as Persist from './Persist';
 import derivationPaths from './constants/derivationPaths';
-import networks from './constants/networks';
+import coin from './constants/coin';
 import { addressAPI } from './AddressAPI';
+import { transactionAPI } from './TransactionAPI';
 
 class Wallet {
   async init() {
@@ -127,12 +137,13 @@ class Wallet {
     return derivedKeys;
   }
 
+  _getAddressesFromKeys(derivedKeys: any[]) {
+    return derivedKeys.map((key: { address: any }) => key.address);
+  }
+
   async initWallet(bip39Mnemonic: string, password: string) {
     const seed = this._mnemonicToSeedSync(bip39Mnemonic, password);
-    const bip32RootKey = this._getBIP32RootKeyFromSeed(
-      seed,
-      networks.BITCOIN_SV
-    );
+    const bip32RootKey = this._getBIP32RootKeyFromSeed(seed, coin.BITCOIN_SV);
     const bip32ExtendedKey = this._getBIP32ExtendedKey(
       this._getDerivationPath(),
       bip32RootKey
@@ -183,7 +194,7 @@ class Wallet {
     prevOutputs: any[] = [],
     nextCursor?: number
   ): Promise<any> {
-    const addressess = keys.map((key: { address: any }) => key.address);
+    const addressess = this._getAddressesFromKeys(keys);
     const data: {
       outputs: any[];
       nextCursor: number;
@@ -212,31 +223,129 @@ class Wallet {
     amountInSatoshi: number,
     transactionFee: number
   ) {
-    console.log(receiverAddress);
-    console.log(amountInSatoshi);
+    const derivedKeys = await Persist.getDerivedKeys();
+    const addressess = this._getAddressesFromKeys(derivedKeys);
+    const utxos = await addressAPI.getUTXOsByAddresses(addressess);
+    Persist.setUtxos(utxos);
+    const targets = [
+      { address: receiverAddress, value: Number(amountInSatoshi) },
+    ];
+    const transactionHex = await this._createSendTransaction(
+      'cN6NafxmNHmhhF6uUug2VuagYm5DWMhRQ5qZDLHyd7Sinbji69Ui',
+      utxos,
+      targets,
+      transactionFee
+    );
+    await transactionAPI.broadcastRawTransaction(transactionHex);
+  }
+
+  async _createSendTransaction(
+    privateKey: string,
+    utxos: [],
+    targets: any[],
+    transactionFee: number
+  ): Promise<string> {
+    console.log(privateKey);
     console.log(transactionFee);
-    // try {
-    //   const utxos = outputs.filter(output => output.spendInfo === null);
-    //   const targets = [
-    //     { address: receiverAddress, value: Number(amountInSatoshi) },
-    //   ];
-    //   const derivedKey = derivedKeys.find(
-    //     derivedKey => derivedKey.isUsed === false
-    //   );
-    //   // });
-    //   console.log(derivedKey);
-    //   // var inputs = utxos.slice(0, 5).map((element) => {
-    //   //   return { ...element, value: 5000 };
-    //   const transactionHex = await utils.createSendTransaction(
-    //     'cN6NafxmNHmhhF6uUug2VuagYm5DWMhRQ5qZDLHyd7Sinbji69Ui',
-    //     utxos,
-    //     targets,
-    //     transactionFee
-    //   );
-    //   return await transactionAPI.broadcastRawTransaction(transactionHex);
-    // } catch (error) {
-    //   throw error;
-    // }
+    const key = ECPair.fromPrivateKey(
+      Buffer.from(
+        '0f4396a043a09537e8712f49828863552bfb0ee7f763fc0c0e06f5ace8da133a',
+        'hex'
+      ),
+      { network: networks.regtest }
+    );
+    const feeRate = 5; // satoshis per byte
+    let { inputs, outputs, fee } = coinSelect(utxos, targets, feeRate);
+    // the accumulated fee is always returned for analysis
+    console.log(fee);
+    // .inputs and .outputs will be undefined if no solution was found
+    if (!inputs || !outputs) throw new Error('Empty inputs || outputs');
+
+    const psbt = new Psbt({ network: networks.regtest });
+    psbt.setVersion(1);
+    const txIds = inputs.map(
+      (input: { outputTxHash: any }) => input.outputTxHash
+    );
+    const rawTxsResponse = await transactionAPI.getRawTransactionsByTxIDs(
+      txIds
+    );
+    const inputsWithRawTxs = rawTxsResponse.rawTxs.map((rawTx: any) => {
+      const base64Decode = atob(rawTx.txSerialized);
+      const decompressed = pako.ungzip(base64Decode);
+      const hex = Buffer.from(decompressed).toString('hex');
+      return { ...rawTx, hex };
+    });
+
+    // const payment = payments.p2pkh({
+    //   pubkey: key.publicKey,
+    //   network: networks.regtest,
+    // });
+    // const output = payment.pubkey;
+
+    let merged = [];
+    for (let i = 0; i < inputs.length; i++) {
+      merged.push({
+        ...inputs[i],
+        ...inputsWithRawTxs.find(
+          (element: { txId: any }) => element.txId === inputs[i].outputTxHash
+        ),
+      });
+    }
+    merged.forEach(
+      (input: { outputTxHash: any; outputIndex: any; hex: any }) => {
+        psbt.addInput({
+          hash: input.outputTxHash,
+          index: input.outputIndex,
+          nonWitnessUtxo: Buffer.from(input.hex, 'hex'),
+          sighashType: 0x41,
+          // redeemScript: output,
+        });
+      }
+    );
+    outputs.forEach((output: { address: any; value: any }) => {
+      // watch out, outputs may have been added that you need to provide
+      // an output address/script for
+      if (!output.address) {
+        output.address = 'mnPbBBvj9JPJ4RHJfWLSwFDpfRCP81F1Zr';
+      }
+      psbt.addOutput({
+        address: output.address,
+        value: output.value,
+      });
+    });
+    psbt.signAllInputs(key, [0x41]);
+    psbt.validateSignaturesOfAllInputs();
+    psbt.finalizeAllInputs();
+    const transactionHex = psbt.extractTransaction().toHex();
+    const base64 = Buffer.from(transactionHex).toString('base64');
+    console.log(base64);
+    return base64;
+
+    /*var tx = new TransactionBuilder(networks.regtest);
+      inputs.forEach(
+        (input: {
+          outputTxHash: string | Buffer | Transaction;
+          outputIndex: number;
+        }) => {
+          tx.addInput(input.outputTxHash, input.outputIndex);
+        }
+      );
+      outputs.forEach((output: { address: any; value: any }) => {
+        // watch out, outputs may have been added that you need to provide
+        // an output address/script for
+        if (!output.address) {
+          output.address = 'mnPbBBvj9JPJ4RHJfWLSwFDpfRCP81F1Zr';
+        }
+        tx.addOutput(output.address, output.value);
+      });
+      tx.sign(0, key);
+      tx.sign(1, key);
+      tx.sign(2, key);
+      const transactionHex = tx.build().toHex();
+      const base64 = Buffer.from(transactionHex).toString('base64');
+      const compressedHex = Buffer.from(base64).toString('utf-8');
+      return compressedHex;
+    */
   }
 }
 
--- lib/nipkow-sdk/src/index.ts
+++ lib/nipkow-sdk/src/index.ts
@@ -1,4 +1,4 @@
-import * as networks from './constants/networks';
+import * as coin from './constants/coin';
 import * as derivationPaths from './constants/derivationPaths';
 import wallet from './Wallet';
 import * as persist from './Persist';
@@ -11,6 +11,6 @@ export { merkleBranchAPI } from './MerkleBranchAPI';
 export { scriptHashAPI } from './ScriptHashAPI';
 export { transactionAPI } from './TransactionAPI';
 export { userAPI } from './UserAPI';
-export { networks, derivationPaths };
+export { coin, derivationPaths };
 export { wallet };
 export { persist };
