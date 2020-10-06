4e8db8696a3ee8431155a04249728bbbf69dd487
shubendrak <shubendrak@gmail.com>
2020-10-06 10:57:37 +0530

    update dashboard tx layout

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import './shared/css/blockheight.css';
 import './shared/css/mnemonic.css';
 import './shared/css/transaction.css';
 import './shared/css/wallet.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'semantic-ui-css/semantic.min.css';
 import './shared/css/main.css';
 
