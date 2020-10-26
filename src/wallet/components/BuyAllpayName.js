a1235d8bfa32ede40612617be2d867dbe27a767a
HarishX <harish@xoken.org>
2020-10-26 15:52:25 +0530

    misc additions

--- src/wallet/components/BuyAllpayName.js
+++ src/wallet/components/BuyAllpayName.js
@@ -4,7 +4,50 @@ import { Button, Form, Modal } from 'semantic-ui-react';
 import * as authActions from '../../auth/authActions';
 
 class BuyAllpayName extends React.Component {
-  onSearch = async () => {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchedName: '',
+      searchResultsSuggestions: false,
+      namespace: false,
+    };
+  }
+
+  onSearch = () => {
+    this.setState({ searchResultsSuggestions: true });
+  };
+
+  searchResultsSuggestionsArea = () => {
+    const { namespace, searchedName } = this.state;
+    function isnamespace() {
+      if (namespace) {
+        return <Checkbox label='Add reseller Rights' defaultChecked />;
+      } else {
+        return <Checkbox label='Add reseller Rights' />;
+      }
+    }
+    if (searchResultsSuggestions) {
+      return (
+        <>
+          <Grid.Row columns={1}>
+            <Grid.Column width={16}>
+              <Grid>
+                <Grid.Row columns={1}>
+                  <Grid.Column width={8}>{searchedName} is available</Grid.Column>
+                  <Grid.Column width={8}>
+                    <Button>Buy</Button>
+                    {isnamespace()}
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Grid.Column>
+          </Grid.Row>
+        </>
+      );
+    } else {
+      <></>;
+    }
+  };
 
   render() {
     return (
@@ -12,20 +55,28 @@ class BuyAllpayName extends React.Component {
         <Modal.Header>Buy Allpay name</Modal.Header>
         <Modal.Content>
           <Modal.Description>
-            <Form>
-              <Form.Group>
-                <Form.Field
-                  label='Name to be purchased'
-                  placeholder='Enter a name you want to purchase'
-                />
-                <Form.Field
-                  control={Button}
-                  content='Search'
-                  label='Search'
-                  onClick={this.onSearch}
-                />
-              </Form.Group>
-            </Form>
+            <Grid>
+              <Grid.Row columns={1}>
+                <Grid.Column width={16}>
+                  <Form>
+                    <Form.Group>
+                      <Form.Field
+                        label='Name to be purchased'
+                        placeholder='Enter a name you want to purchase'
+                      />
+                      <Form.Field
+                        control={Button}
+                        content='Search'
+                        label='Search'
+                        onClick={this.onSearch}
+                      />
+                      <Checkbox label='Namespace' onChange={this.setState({ namespace: true })} />
+                    </Form.Group>
+                  </Form>
+                </Grid.Column>
+              </Grid.Row>
+              {this.searchResultsSuggestionsArea}
+            </Grid>
           </Modal.Description>
         </Modal.Content>
       </>
