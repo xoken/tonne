48133f58b07688e69858dfc5f9440b5fbafcee47
shubendrak <shubendrak@gmail.com>
2021-01-12 18:13:42 +0530

    update deps

--- src/allpay/components/ProgressIndicator.js
+++ src/allpay/components/ProgressIndicator.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, css } from 'aphrodite/no-important';
 
 const stepSpacing = 5;
 const stepIndicatorSize = 10;
@@ -14,9 +13,7 @@ export default class ProgressIndicator extends React.Component {
     return Array.apply(null, {
       length: steps,
     }).map((step, index) => (
-      <li
-        key={'Progress Step ' + index}
-        className={`progress-indicator-step ${css(styles.progressIndicatorStep)}`}></li>
+      <li key={'Progress Step ' + index} className='progress-indicator-step'></li>
     ));
   }
 
@@ -26,23 +23,23 @@ export default class ProgressIndicator extends React.Component {
       <div className='progress-indicator'>
         <ul className='progress-indicator-list'>{this.renderSteps(steps)}</ul>
         <span
-          className={`progress-indicator-fill ${css(styles.progressIndicatorFill)}`}
+          className='progress-indicator-fill'
           style={{ height: this.activeHeight(activeStep) }}></span>
       </div>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  progressIndicatorStep: {
-    width: stepIndicatorSize,
-    height: stepIndicatorSize,
-  },
-  progressIndicatorFill: {
-    width: stepIndicatorSize,
-    height: stepIndicatorSize,
-  },
-});
+// const styles = StyleSheet.create({
+//   progressIndicatorStep: {
+//     width: stepIndicatorSize,
+//     height: stepIndicatorSize,
+//   },
+//   progressIndicatorFill: {
+//     width: stepIndicatorSize,
+//     height: stepIndicatorSize,
+//   },
+// });
 
 ProgressIndicator.defaultProps = {
   activeStep: 1,
