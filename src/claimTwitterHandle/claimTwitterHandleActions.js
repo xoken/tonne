b7be6681d71572ec16a12ddf73e6b349af9f90ea
shubendrak <shubendrak@gmail.com>
2021-01-31 17:55:35 +0530

    debug popup build 1

--- src/claimTwitterHandle/claimTwitterHandleActions.js
+++ src/claimTwitterHandle/claimTwitterHandleActions.js
@@ -14,12 +14,12 @@ export const doTwitterAuth = history => async (dispatch, getState, { serviceInje
     'twitter-auth-window',
     'width=500, height=500, top=0, left=0'
   );
+  console.log('Popup open');
   //   this.polling(popup);
   const messageHandler = async event => {
-    console.log('event.origin : ' + event.origin);
-    console.log('window.location.origin : ' + window.location.origin);
-    console.log('event.souce.name : ' + event.source.name);
     if (event.origin !== window.location.origin) return;
+    console.log('event.source.name : ' + event.source.name);
+    debugger;
     if (event.source.name === 'twitter-auth-window') {
       const queryParams = new URLSearchParams(event.data);
       const oauthToken = queryParams.get('oauth_token');
