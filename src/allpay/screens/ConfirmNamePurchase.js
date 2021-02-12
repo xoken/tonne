589b67b84c7424ff98d614c93b9801749bf8f923
shubendrak <shubendrak@gmail.com>
2021-02-12 12:00:38 +0530

    getAllpayHandle on buy

--- src/allpay/screens/ConfirmNamePurchase.js
+++ src/allpay/screens/ConfirmNamePurchase.js
@@ -5,6 +5,7 @@ import { Button, Grid, Header } from 'semantic-ui-react';
 import { utils } from 'allegory-allpay-sdk';
 import RenderTransaction from '../components/RenderTransaction';
 import * as allpayActions from '../allpayActions';
+import * as walletActions from '../../wallet/walletActions';
 import * as authActions from '../../auth/authActions';
 
 class ConfirmNamePurchase extends React.Component {
@@ -41,6 +42,7 @@ class ConfirmNamePurchase extends React.Component {
         );
         if (txBroadcast) {
           this.setState({ isError: false, message: 'Transaction signed and relayed successfully' });
+          await dispatch(walletActions.getAllpayHandle());
           await dispatch(
             authActions.updateProfileName(profile.screenName, utils.codePointToName(outpoint.name))
           );
--- src/wallet/components/ReceiveTransaction.js
+++ src/wallet/components/ReceiveTransaction.js
@@ -15,7 +15,6 @@ class ReceiveTransaction extends React.Component {
 
   async componentDidMount() {
     const { dispatch } = this.props;
-    await dispatch(walletActions.getAllpayHandle());
     await dispatch(walletActions.getUsedAddresses());
     await dispatch(walletActions.getUnusedAddresses());
   }
